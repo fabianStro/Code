128a2183373202e8c1249b2245403de694e91871
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-05 12:14:16 +0200

    add Ende print

--- dart/hello.dart
+++ dart/hello.dart
@@ -4,7 +4,7 @@ void main () {
   int counter;
   double zahl = 12.83;
   const double pi = 3.1415926535897932;
-  String greet = "Hello\n  This is my first dart programm";
+  String greet = "Hello\n ¥This is my first dart programm";
   var adresse = {'Fabian' , '\n  Strottmann' , '\n  Schilfkamp 20' , '\n  30851 Langehagen'};
 
   print("\n $greet \n");
